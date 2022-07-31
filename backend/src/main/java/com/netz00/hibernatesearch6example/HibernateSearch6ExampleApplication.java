56bcf787efc30f317693db20169fa0c1f02ddf82
bozo.durdov <bozo.durdov@mail.tn.mes>
2022-07-31 22:28:23 +0200

    Spring Boot project initialized

---
+++ backend/src/main/java/com/netz00/hibernatesearch6example/HibernateSearch6ExampleApplication.java
@@ -0,0 +1,13 @@
+package com.netz00.hibernatesearch6example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateSearch6ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateSearch6ExampleApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/netz00/hibernatesearch6example/HibernateSearch6ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netz00.hibernatesearch6example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateSearch6ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
