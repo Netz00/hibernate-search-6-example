93e86d01079b2913626b5bce941d4134e797102e
Netz <bozo2000d@gmail.com>
2022-09-30 21:21:13 +0200

    Added H2 for testing

--- backend/src/test/java/com/netz00/hibernatesearch6example/HibernateSearch6ExampleApplicationTests.java
+++ backend/src/test/java/com/netz00/hibernatesearch6example/HibernateSearch6ExampleApplicationTests.java
@@ -1,9 +1,11 @@
 package com.netz00.hibernatesearch6example;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class HibernateSearch6ExampleApplicationTests {
 
 	@Test
